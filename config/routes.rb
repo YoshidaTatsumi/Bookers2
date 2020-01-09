5af9d20643c40fbd6a8f0683973e64f754c74ade
そそ <sora@sosonoMacBook-Air.local>
2020-01-09 15:15:19 +0900

    [Add]book-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:create, :index, :show, :destroy, :update]
+  resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:show, :edit, :update]
 end
