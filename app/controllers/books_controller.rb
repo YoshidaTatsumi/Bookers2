ab7afff005820a7553bab08c49c4e8c69227ac89
そそ <sora@sosonoMacBook-Air.local>
2020-01-09 13:41:39 +0900

    [Add]destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,12 @@ class BooksController < ApplicationController
 		@books = Book.all
 	end
 	def show
+		@book = Book.find(params[:id])
+	end
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to books_path
 	end
 	private
 	def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:create, :index, :show]
+  resources :books, only: [:create, :index, :show, :destroy, :update]
 end
