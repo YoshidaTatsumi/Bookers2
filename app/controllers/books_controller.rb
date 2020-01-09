a1fe02b65019497027a720432ddbc48da2e90c05
そそ <sora@sosonoMacBook-Air.local>
2020-01-09 18:01:04 +0900

    [Add]book-update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,16 @@ class BooksController < ApplicationController
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		@book.save
-		redirect_to books_path
+		redirect_to book_path(@book)
 	end
 	def index
 		@book = Book.new
 		@books = Book.all
+		@user = current_user
 	end
 	def show
-		@book = Book.find(params[:id])
+		@books = Book.find(params[:id])
+		@book = Book.new
 	end
 	def edit
 		@book = Book.find(params[:id])
