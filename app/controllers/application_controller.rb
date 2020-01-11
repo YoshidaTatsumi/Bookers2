132f18ab766e64430f704b07e96e3c31ab60361b
そそ <sora@sosonoMacBook-Air.local>
2020-01-11 13:29:08 +0900

    [Add]login-username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!, except: [:top]
   	protected
   	def configure_permitted_parameters
+      added_attrs = [ :email, :name, :password, :password_confirmation ]
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   	end
   	def after_sign_in_path_for(resource)
   		user_path(current_user.id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:name]
 
   has_many :books, dependent: :destroy
   attachment :profile_image
-  validates :name, presence: true, length: {in: 2..20}
+  validates :name, presence: true, length: {in: 2..20}, uniqueness: true
+
+  #usernameを利用してログインするようにオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["name = :value", { :value => name }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  #emailを不要とする
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
